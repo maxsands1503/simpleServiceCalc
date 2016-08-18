20d76415bac5c4b1490700da2a231a987f52693f
Maxwell Sands <maxwellsands@Maxwells-MacBook-Air.local>
2016-08-18 09:50:37 -0600

    Done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,8 @@
-trig.controller("TrigController", function(){
-
-  
+trig.controller("TrigController", function($scope, CalculatorFactory){
+  $scope.hypotenuse;
+  $scope.calculate = function(sideA, sideB){
+    $scope.hypotenuse = CalculatorFactory.calculate(sideA, sideB)
+    return $scope.hypotenuse;
+  }
 
 });
--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,14 @@
 trig.factory("CalculatorFactory", function(){
 
+return {
+    calculate : function(inputA, inputB){
+    var a2 = Number(inputA * inputA)
+    var b2 = Number(inputB * inputB);
+    var c2 = (Number(a2)+ Number(b2));
 
+    return Math.sqrt(c2);
+  }
+}
 
 
 
